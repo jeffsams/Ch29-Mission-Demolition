79ea0613e9704f387829a7a4fe1e02a7a18a7785
jeff john <jeffsams@gmail.com>
2017-06-25 20:33:35 -0400

    FollowCam return Bugfix

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -40,24 +40,17 @@ public class FollowCam : MonoBehaviour {
                 }
             }
         }
-        // if there's only one line following an if, it doesnt need braces
-       
-        // Get the position of the poi
 
-	}
-
-    void Update()
-    {
-        if (poi == null) return; // return if there is no poi
-
-        Vector3 destination = poi.transform.position;
         destination.x = Mathf.Max(minXY.x, destination.x);
         destination.y = Mathf.Max(minXY.y, destination.y);
         destination = Vector3.Lerp(transform.position, destination, easing);
-        //Retain a destination.z of camZ
         destination.z = camZ;
-        //Set the camera to the destination
         transform.position = destination;
         this.GetComponent<Camera>().orthographicSize = destination.y + 10;
+    }
+
+    void Update()
+    {
+
     }
 }
