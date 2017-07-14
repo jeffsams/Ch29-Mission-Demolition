ad587f92944422ff2363a4c8c61d3dd7655cafe5
jeff john <jeffsams@gmail.com>
2017-07-14 10:45:06 -0400

    Changed Version to 5.5.0

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour {
+    static public FollowCam S; //singleton
+    public float easing = .05f;
+    public bool _____________________;
+    public Vector2 minXY;
+
+    //fields set dynamically 
+    public GameObject poi;
+    public float camZ;
+
+    void Awake()
+    {
+        S = this;
+        camZ = this.transform.position.z;
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 destination;
+        if (poi == null)
+        {
+            destination = Vector3.zero;
+        } else
+        {
+            destination = poi.transform.position;
+            if (poi.tag == "Projectile")
+            {
+                if (poi.GetComponent<Rigidbody>().IsSleeping() )
+                {
+                    poi = null;
+                    return;
+                }
+            }
+        }
+
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        destination = Vector3.Lerp(transform.position, destination, easing);
+        destination.z = camZ;
+        transform.position = destination;
+        this.GetComponent<Camera>().orthographicSize = destination.y + 10;
+    }
+
+    void Update()
+    {
+
+    }
+}
